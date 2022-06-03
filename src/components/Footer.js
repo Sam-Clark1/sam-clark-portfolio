fe9da38c16c30a05d5d55098fa93fde9fa6529c5
Sam Clark <samclark2399@gmail.com>
2022-06-02 20:51:23 -0400

    added README

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 import { Box } from "@mui/system";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import Twitter from "@mui/icons-material/Twitter";
 
 export default function Footer() {
     return (
@@ -11,6 +12,9 @@ export default function Footer() {
             </a>
             <a href="https://www.linkedin.com/in/samuel-clark-0a3836205/" target="_blank" rel='noreferrer'>
                 <LinkedInIcon sx={{ fontSize: 75, mr:5, color: 'white' }}/>
+            </a>
+            <a href="https://twitter.com/" target="_blank" rel='noreferrer'>
+                <Twitter sx={{ fontSize: 75, mr:5, color: 'white' }}/>
             </a>
         </Box>
     </Box>
