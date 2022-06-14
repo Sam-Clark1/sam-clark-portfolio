e0f9a5b78d4c6a45e8dfef3c85237eb5074c0ccc
Sam Clark <samclark2399@gmail.com>
2022-06-14 12:37:23 -0400

    updated resume. Installed react-router-dom

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Box, Grid, Typography, Button, Grow } from "@mui/material";
+import resume from '../assets/Samuel Clark Resume.pdf'
 
 export default function Resume() {
     return(
@@ -10,7 +11,7 @@ export default function Resume() {
                         <Typography variant="h4" gutterBottom component="div">Proficiencies:</Typography>
                         <ul>
                             <li>Languages: Javascript, SQL, HTML/CSS</li>
-                            <li>Technologies: Node.js, Express.js, React.js, REST, GraphQL</li>
+                            <li>Technologies: Node.js, Express.js, React.js, REST, GraphQL, JWT, MVC</li>
                             <li>Databases: MySQL, MongoDB</li>
                             <li>Frameworks: Sequelize ORM, Mongoose ODM, Jest, Handlebars.js, JQuery</li>
                             <li>Tools: Git, GitHub, GitLab, Heroku, Postman, Bash</li>
@@ -22,7 +23,7 @@ export default function Resume() {
                 <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
                     <Typography sx={{color:'white', display: 'flex', flexDirection: 'row', my:"15%"}} variant="h4" gutterBottom component="div">
                         Download my resume <Button variant="contained" sx={{backgroundColor:'white', color: '#011936', fontSize:'1rem', ml:2}} size="auto" 
-                        href="https://docs.google.com/document/d/1gcTBg8bQ4trqiHpK3C8jP8gyKRnI7Qev/edit?usp=sharing&ouid=103675949855728027670&rtpof=true&sd=true"
+                        href={resume}
                         target="_blank" rel='noreferrer'>here</Button>
                     </Typography>
                 </Grid>
